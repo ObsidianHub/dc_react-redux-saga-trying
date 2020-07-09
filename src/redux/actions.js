07e5d37997f1bb61af06cda77197e55f8c7760bd
Denny <obsidian.y.dragon@gmail.com>
2020-07-09 23:18:36 +0300

    add Loader

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { CREATE_POST, FETCH_POST } from "./types";
+import { CREATE_POST, FETCH_POST, SHOW_LOADER, HIDE_LOADER } from "./types";
 
 export function createPost(post) {
   return {
@@ -7,12 +7,28 @@ export function createPost(post) {
   };
 }
 
+export function showLoader() {
+  return {
+    type: SHOW_LOADER,
+  };
+}
+
+export function hideLoader() {
+  return {
+    type: HIDE_LOADER,
+  };
+}
+
 export function fetchPosts() {
   return async (dispatch) => {
+    dispatch(showLoader());
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/posts?_limit=5"
     );
     const json = await response.json();
-    dispatch({ type: FETCH_POST, payload: json });
+    setTimeout(() => {
+      dispatch({ type: FETCH_POST, payload: json });
+      dispatch(hideLoader());
+    }, 500);
   };
 }
