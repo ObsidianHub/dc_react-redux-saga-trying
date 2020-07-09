a436e3b7a35b05394cda5a980a534bf07b3dbb32
Denny <obsidian.y.dragon@gmail.com>
2020-07-09 22:52:55 +0300

    realize button for fetched posts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, compose } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import { rootReducer } from "./redux/rootReducer";
+import App from "./App";
 
 const store = createStore(
   rootReducer,
   compose(
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
