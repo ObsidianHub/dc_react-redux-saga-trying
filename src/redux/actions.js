5524f531ce6950967057c8d3fbcc4d913f0cac3f
Denny <obsidian.y.dragon@gmail.com>
2020-07-10 17:23:27 +0300

    end work with redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,11 @@
-import { CREATE_POST, FETCH_POST, SHOW_LOADER, HIDE_LOADER } from "./types";
+import {
+  CREATE_POST,
+  FETCH_POST,
+  SHOW_LOADER,
+  HIDE_LOADER,
+  SHOW_ALERT,
+  HIDE_ALERT,
+} from "./types";
 
 export function createPost(post) {
   return {
@@ -19,16 +26,40 @@ export function hideLoader() {
   };
 }
 
+export function showAlert(text) {
+  return (dispatch) => {
+    dispatch({
+      type: SHOW_ALERT,
+      payload: text,
+    });
+
+    setTimeout(() => {
+      dispatch(hideAlert());
+    }, 1500);
+  };
+}
+
+export function hideAlert() {
+  return {
+    type: HIDE_ALERT,
+  };
+}
+
 export function fetchPosts() {
   return async (dispatch) => {
-    dispatch(showLoader());
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts?_limit=5"
-    );
-    const json = await response.json();
-    setTimeout(() => {
-      dispatch({ type: FETCH_POST, payload: json });
+    try {
+      dispatch(showLoader());
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?_limit=5"
+      );
+      const json = await response.json();
+      setTimeout(() => {
+        dispatch({ type: FETCH_POST, payload: json });
+        dispatch(hideLoader());
+      }, 500);
+    } catch (e) {
+      dispatch(showAlert("Что-то пошло не так"));
       dispatch(hideLoader());
-    }, 500);
+    }
   };
 }
